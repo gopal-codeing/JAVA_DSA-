895c30c1a0b0a6ce6742a76ef4abec31f2a4c53b
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-04-25 23:39:13 +0530

    CirculerLinkedList.java

--- CirculerLinkedList.java
+++ CirculerLinkedList.java
@@ -34,6 +34,7 @@ public class CirculerLinkedList{
       if(tail!=null){
         System.out.print("Yes i am circuler LinkedList");
      }
+     }
      
      public static void main(String []args){
         // System.out.println("Hello, World!");
