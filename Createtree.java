2414a90543924041c480f7ef579f5034fe974678
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-05-12 01:34:57 +0530

    Createtree.java

--- Createtree.java
+++ Createtree.java
@@ -15,20 +15,17 @@ class Node {
 public class Createtree {
     Scanner sc = new Scanner(System.in);
     Node createTree(Node root) {
-        System.out.print("Enter the tree data !");
-        int data = sc.nextInt();
-        root  = new Node(data);
-
-        if(data == -1) {
-            return null;
-        }
-        System.out.print("data insert left : " + data);
-        System.out.println();
-        root.left  = createTree(root.left);
-        System.out.print("data insert right" + data);
-         System.out.println();
-        root.right  = createTree(root.right);
-        return root;
+         System.out.print("Enter the data : ");
+      int data = sc.nextInt();
+          root = new Node(data);
+          if(data ==-1)
+           return root;
+          System.out.println("Enter the insert of the left of "+data);
+          root.left = bildTree(root.left);
+           System.out.println("Enter the insert of the right of "+data);
+          root.right = bildTree(root.right);
+          
+         return root;
     }
 
     public static void main(String []args) {
