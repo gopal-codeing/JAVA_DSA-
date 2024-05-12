f69c9c96f916fa7471126fcc56ecd119b14e36f0
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-05-12 19:09:12 +0530

    Createtree.java

--- Createtree.java
+++ Createtree.java
@@ -31,13 +31,43 @@ public class Createtree{
     }
     // Leveloder traversal 
     //  Leveloder traversal  ko hmm  brith fast search bhi bolte  hai 
-   
+
+   //   print Data  in  tree form
+    // Level odertravesal
+     void tree(Node root){
+            
+            if(root == null){
+                return;
+            }
+            
+              Queue<Node> q= new LinkedList<>();
+              q.add(root);
+               
+             
+              while(!q.isEmpty()){
+                  Node tem = q.poll();
+                  
+
+                  if(tem.data != -1)
+                  System.out.print(tem.data+" ");
+                  
+                  if(tem.left!=null){
+                      q.add(tem.left);
+                  }
+                  
+                  if(tem.right!=null){
+                      q.add(tem.right);
+                  }
+              }
+        }
     
      public static void main(String []args){
         // System.out.println("Level order trareval..!");
         Createtree obj = new Createtree();
         Node root = null ;
         root= obj.bildTree(root);
+       // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1  this a input data;
+        obj.tree(root);
         
         
         
