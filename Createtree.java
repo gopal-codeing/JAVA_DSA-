48d16a1ffab8442211d618820b02514a255f70d0
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-05-13 00:24:59 +0530

    Createtree.java

--- Createtree.java
+++ Createtree.java
@@ -46,8 +46,15 @@ public class Createtree{
              
               while(!q.isEmpty()){
                   Node tem = q.poll();
-                  
-
+                 
+                if(tem == null){
+                 System.out.println();
+                 
+                 if(!q.isEmpty()){
+                     q.add(null);
+                 }
+                }
+                else{
                   if(tem.data != -1)
                   System.out.print(tem.data+" ");
                   
@@ -58,18 +65,60 @@ public class Createtree{
                   if(tem.right!=null){
                       q.add(tem.right);
                   }
+                }
               }
+              System.out.println();
         }
+    
+      void inorderTraversal(Node root){
+          if(root==null){
+              return;
+          }
+          
+          inorderTraversal(root.left);
+          if(root.data!=-1)
+          System.out.print(root.data+" ");
+          inorderTraversal(root.right);
+      }
+      
+      
+      
+        void preOrdertraversal(Node root){
+          if(root==null){
+              return;
+          }
+          
+          if(root.data!=-1)
+          System.out.print(root.data+" ");
+          preOrdertraversal(root.left);
+          preOrdertraversal(root.right);
+      }
+      
+      void postOrdertraversal(Node root){
+          if(root==null){
+              return;
+          }
+          postOrdertraversal(root.left);
+          postOrdertraversal(root.right);
+          if(root.data != -1)
+          System.out.print(root.data+" ");
+      }
     
      public static void main(String []args){
         // System.out.println("Level order trareval..!");
         Createtree obj = new Createtree();
         Node root = null ;
         root= obj.bildTree(root);
-       // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1  this a input data;
-        obj.tree(root);
-        
-        
-        
+       // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1  this a input data
+        obj.tree(root); // this is leveloder traversal  
+        System.out.println();
+        System.out.print("InoderTraversal Data :");
+        obj.inorderTraversal(root);  
+        System.out.println();
+        System.out.print("PreorderTraversal Data :");
+        obj.preOrdertraversal(root);
+          System.out.println();
+        System.out.print("PostorderTraversal Data :");
+        obj.postOrdertraversal(root);
      }
 }
